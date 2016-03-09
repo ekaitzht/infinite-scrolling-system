d36fb2720a2bb36026513b8f9b05898102d2fd2b
Ekaitz <ekaitz@murnow.com>
2016-03-09 10:07:23 +0100

    Initial code: added currency filter; added font-size; relative date;

---
+++ static/js/filters.js
@@ -0,0 +1,19 @@
+angular.module('asciiFilters',[]).
+   filter('dateFilter',  function() {
+    return function(date) {
+	    var today = new Date();
+	    var dateItem = new Date(date);
+	    var timeDiff = Math.abs(today.getTime() - dateItem.getTime());
+		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+	    
+	    if ( diffDays > 7){
+			return date;
+			
+		} else {
+			return diffDays + ' days ago';
+		}
+		 
+    };
+  });
+
+ 
